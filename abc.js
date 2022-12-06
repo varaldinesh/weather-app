1d7e502472e8cf66d4f5edde5660cc36d1b79add
Dinesh <varaldinesh0@gmail.com>
2022-12-07 02:42:49 +0530

    commit 1.1

--- abc.js
+++
@@ -1,6 +0,0 @@
-max1.innerHTML = Math.round((response.forecasts[1].high - 32) / 1.8);
-max2.innerHTML = Math.round((response.forecasts[2].high - 32) / 1.8);
-max3.innerHTML = Math.round((response.forecasts[3].high - 32) / 1.8);
-max4.innerHTML = Math.round((response.forecasts[4].high - 32) / 1.8);
-max5.innerHTML = Math.round((response.forecasts[5].high - 32) / 1.8);
-max6.innerHTML = Math.round((response.forecasts[6].high - 32) / 1.8);
